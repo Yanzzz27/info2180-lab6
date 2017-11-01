224e7babe96311aef37e52446efeb348e9b45908
Yanz <jusyanzzz27@gmail.com>
2017-11-01 18:17:00 +0000

    EX3

--- def.js
+++ def.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    $('#search').click(function(){ 
+    $('#Sbutton').click(function(){ 
         $.ajax ({
             type: 'GET',
             url: 'request.php',
-            data: {q: 'definition'},
+            data: {q: $("#input").val()},
             success: function(data){
-                alert(data);
+                $("#result").html(data);
             }
         })
     
