cc18497360728b007ba801c314c201e2c0d37229
Yanz <jusyanzzz27@gmail.com>
2017-11-02 01:21:28 +0000

    EX4

--- def.js
+++ def.js
@@ -1,3 +1,4 @@
+/* global $*/
 $(document).ready(function(){
     $('#Sbutton').click(function(){ 
         $.ajax ({
@@ -7,7 +8,30 @@ $(document).ready(function(){
             success: function(data){
                 $("#result").html(data);
             }
-        })
+        });
     
-    })
+    });
+    
+    $('#Abutton').click(function(){ 
+        $.ajax ({
+            type: 'GET',
+            url: 'request.php',
+            data: {q: '&all=true'},
+            success: function(data){
+                $('#result').html("");
+                $("#result").append('<ol></ol>');
+                $(data).find('definition').each(function(){
+                    var store= '<h3>'+$(this).find("name").text()+'</h3>';
+					store += '<p>'+$(this).find("meaning").text()+'</p>';
+					store += '<p>'+"-"+$(this).find("author").text()+'</p>';
+					$("#result ol").append('<li>'+store+'</li>');
+                    
+                });
+            }
+        });
+    
+    });
+    
+    
+    
 })
--- request.php
+++ request.php
@@ -13,6 +13,48 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
-
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition>
+        <name>definition</name>
+        <author>Yanique</author>
+        <meaning>A statement of the exact meaning of a word, especially in a dictionary.</meaning>
+    </definition>
+    <definition>
+        <name>bar</name>
+        <author>Mark</author>
+        <meaning>A place that sells alcholic beverages</meaning>
+    </definition>
+    <definition>
+        <name>ajax</name>
+        <author>Patrice</author>
+        <meaning>Technique which involves the use of javascript and xml (or JSON)</meaning>
+    </definition>
+    <definition>
+        <name>html</name>
+        <author>Tonika</author>
+        <meaning>The standard markup language for creating web pages and web applications.</meaning>
+    </definition>
+    <definition>
+        <name>css</name>
+        <author>Dhanique</author>
+        <meaning>A style sheet language used for describing the presentation of a document written in a markup language.</meaning>
+    </definition>
+    <definition>
+        <name>javascript</name>
+        <author>Michelle</author>
+        <meaning>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</meaning>
+    </definition>
+    <definition>
+        <name>php</name>
+        <author>Shamona</author>
+        <meaning>A server-side scripting language, and a powerful tool for making dynamic and interactive websites</meaning>
+    </definition>
+</entries>';
+if ($query=="&all=true")
+{header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();}
+else
+{print "<h3>" . strtoupper($query) . "</h3>";
+print "<p>" . $definition[$query] . "</p>";}
